198248353c36bbba0c5e24de9575f9db7221c918
nono26152 <nono26152@users.noreply.github.com>
2016-09-09 12:00:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,19 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+var pg = require('pg');
 
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 const restService = express();
 restService.use(bodyParser.json());
